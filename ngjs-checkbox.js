fc1813f233631436e7e286351f0f63203f1e5ae6
pbrizardo <pbrizardo@gmail.com>
2019-08-14 10:10:18 -0700

    Update ngjs-checkbox.js

--- ngjs-checkbox.js
+++ ngjs-checkbox.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  angular.module('myApp')
+  angular.module('ngjsCheckbox', [])
     .directive('ngjsCheckbox', function() {
       return {
         restrict: 'A',
